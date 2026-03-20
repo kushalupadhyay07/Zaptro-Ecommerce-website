e98d1dab91edfc85f5301ec39ae8d31d61c9273b
kushal upadhyay <kushalupadhyay1818@gmail.com>
2026-03-20 21:31:19 +0530

    Update About Page

--- src/websitepages/About.jsx
+++ src/websitepages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import Lottie from "lottie-react";
+import aboutus from "../assets/aboutus.json";
 export default function About() {
   return (
-    <div>
-      About
+    <div className="flex h-screen">
+      <div className="w-5/10 ">
+        <Lottie animationData={aboutus} style={{ height: "100%" }}></Lottie>
+      </div>
+      <div
+        className="w-5/10  flex flex-col items-center gap-5  "
+        style={{ padding: "90px" }}
+      >
+        <h1 className="text-2xl font-bold border-b-1"> About us </h1>
+        <p className="text-xl font-semibold">
+          Welcome to our platform! We are dedicated to providing high-quality
+          products at affordable prices. Our mission is to make online shopping
+          easy, fast, and reliable for everyone. We focus on delivering the best
+          user experience with secure payments and fast delivery. We carefully
+          select our products to ensure quality and customer satisfaction. Thank
+          you for choosing us!
+        </p>
+      </div>
     </div>
-  )
+  );
 }
